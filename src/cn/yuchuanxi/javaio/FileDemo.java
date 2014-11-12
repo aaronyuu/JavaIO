7f42a852ac721b0e1bde33d67d8f4ede5542cc24
vcvok <vcvok@qq.com>
2014-11-12 18:21:19 +0800

    2014年11月12日18:21:15

--- src/cn/yuchuanxi/javaio/FileDemo.java
+++ src/cn/yuchuanxi/javaio/FileDemo.java
@@ -1,25 +1,34 @@
 ﻿package cn.yuchuanxi.javaio;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.RandomAccessFile;
 
 public class FileDemo {
 
 	/**
-	 * @throws IOException 
-	 * File
-	 * FileOutputStream
-	 * FileInputStream
-	 * FileWriter
-	 * FileReader
-	 * BufferedReader
+	 * RandomAccessFile(file, "rw") 有读/写的方法   最基础的读写类
+	 *  FileOutputStream(file)
+	 *  FileInputStream(file)
+	 *  DataOutputStream(OutputStream out) 写/输出流
+	 * BufferedOutputStream
+	 * BufferedInputStream
+	 *  FileWriter(file)
+	 *  FileReader(file)
+	 *  BufferedWriter(Writer out)  相比FileWrite(),好处是有缓存,减少磁盘写次数.
+	 *  BufferedReader(Reader in) 有个readLine()方法很实用
+	 *  
 	 */
 	public static void main(String[] args) throws IOException {
 		/**
@@ -41,15 +50,8 @@ public class FileDemo {
 				file.createNewFile();
 			}
 			
-			
 			/**
-			 * RandomAccessFile(file, "rw") 有读/写的方法
-			 *  FileOutputStream(file)
-			 *  FileInputStream(file)
-			 *  FileWriter(file)
-			 *  FileReader(file)
-			 *  BufferedReader(fr)
-			 *  
+			 * RandomAccessFile 可读,可写,最基础的读写类
 			 */
 			RandomAccessFile raf= new RandomAccessFile(file, "rw");
 			raf.write('b');
@@ -66,25 +68,76 @@ public class FileDemo {
 			System.out.println("读到的十进制数据"+r+",转换成字符"+c);
 			raf.close();
 			
-			
+			/**
+			 * FileOutputStream
+			 * FileInputStream
+			 * 相对于RandomAccessFile,貌似没什么优势.
+			 */
 			FileOutputStream fos=new FileOutputStream(file);
 			fos.write("a".getBytes());
 			fos.write("中文试试".getBytes());
 			/**
 			 * getBytes("GBK") ,类型要和当前文本的字符编码格式一致.
-			 * 本例中,getBytes("utf-16be")是乱码.
+			 * 本例中,getBytes("gbk")是乱码.
 			 */
 			fos.write("中文试试UTF-8".getBytes("utf-8"));
 			fos.write("中文试试GBK".getBytes("gbk"));
 			fos.close();
-			
-			
 			FileInputStream fis = new FileInputStream(file);
 			byte[] str1 = new byte[1024];
 			int strlength;
 			strlength=fis.read(str1,0,str1.length);
 			System.out.println(new String(str1,0,strlength));
 			fis.close();
+			
+			
+			
+			/**
+			 * 对FileOutputStream的包装,
+			 *通过writeInt(),writeChar(),readInt(),readChar()等方法, 
+			 *实现整型,字符型等数据类型的直接读写.
+			 * 	DataOutputStream(OutputStream out) 写/输出流
+			 * writeInt(int);
+			 * DataInputStream 读/输入流
+			 * readInt()
+			 * 
+			 */
+			DataOutputStream dos=new DataOutputStream(new FileOutputStream(file));
+			char wChar='c';
+			dos.writeChar(wChar);
+			long wLong=123L;
+			dos.writeLong(wLong);
+			dos.close();
+			
+			DataInputStream dis=new DataInputStream(new FileInputStream(file));
+			char rChar =dis.readChar();
+			long rLong=dis.readLong();
+			System.out.println("通过DataInputStream获取的字符:"+rChar);
+			System.out.println("通过DataInputStream.readLong()获取的长整型:"+rLong);
+			dis.close();
+			
+			/**
+			 * BufferedOutputStream
+			 * BufferedInputStream
+			 */
+			BufferedOutputStream bos=new BufferedOutputStream(new FileOutputStream(file));
+			bos.write("测试BufferedOutputStream".getBytes());
+			bos.close();
+			
+			BufferedInputStream bis=new BufferedInputStream(new FileInputStream(file));
+			int bisReadLength=0;
+			byte[] bisReadByte=new byte[1024];
+			bisReadLength=bis.read(bisReadByte,0,bisReadByte.length);
+			System.out.println(new String(bisReadByte,0,bisReadLength));
+			bis.close();
+			
+			/**
+			 *  FileWriter(file)
+			 *  FileReader(file)
+			 *  
+			 *  BufferedWriter(Writer out)  相比FileWrite(),好处是有缓存,减少磁盘写次数.
+			 *  BufferedReader(Reader in) 有个readLine()方法很实用
+			 */
 			
 			FileWriter fw=new FileWriter(file);
 			fw.write("hahaa测试中文,");
@@ -100,18 +153,13 @@ public class FileDemo {
 			fw.close();
 			bw.close();
 			
-			/**
-			 * 读操作
-			 */
 			FileReader fr=new FileReader(file);
 			BufferedReader br=new BufferedReader(fr);
 			System.out.println(br.readLine());
 			br.close();
 			fr.close();
 		
-			/**
-			 * 测试文字编码转换
-			 */
+		
 				 
 			 
 			 
---
+++ src/cn/yuchuanxi/javaio/IOUtils.java
@@ -0,0 +1,5 @@
+package cn.yuchuanxi.javaio;
+
+public class IOUtils {
+
+}
